0d9caa9edb8524d0185159535c129f978beaeac5
Your Name <09taumang@gmail.com>
2023-03-29 16:16:21 +0200

    still for testing

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
+import React, {useEffect, useLayoutEffect,useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Image, TextInput } from 'react-native';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
+import {createStackNavigation} from '@react-navigation/stack';
 
 export default function App() {
+
+  //Two states which are added for the password and email inputs from the user.
+  const [emil,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+
+      {/* Image or logo being added here */}
+      <Image 
+      source = {require('./img/crop_logo.png')}
+      style = {styles.image}
+      />
+      {/* Default text which comes with the react native installation package */}
+      <Text>something for now</Text>
+
+
+    <View style={styles.inputView}>
+      <TextInput 
+      style={styles.TextInput}
+      placeholder='Email'
+      placeholderTextColor='black'
+      onChangeText={(email)=> setEmail(email)}/>
+    </View>
+    <View style={styles.inputView}>
+      <TextInput 
+      style={styles.TextInput}
+      placeholder='Password'
+      placeholderTextColor='black'
+      secureTextEntry={true}
+      onChangeText={(password)=> setPassword(password)}/>
+    </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    marginBottom:250,
+    // marginTop:120,
+  },
+  inputView: {
+    backgroundColor:'grey',
+    borderRadius: 20,
+    width:'70%',
+    height: 45,
+    marginBottom:20,
+    alignItems:'center'
+  },
+  TextInput:{
+    height:50,
+    flex:1,
+    padding:10,
+    marginLeft:20,
+  }
 });
